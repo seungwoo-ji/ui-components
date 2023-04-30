72ea423a852ddf324838fb2b56e8d6cdc4727105
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2023-04-29 21:20:59 -0400

    Added updateProgressBar method

--- interactive-pricing-component/src/index.js
+++ interactive-pricing-component/src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+class App {
+  // HTML elements
+  rangeInputEl = document.querySelector("input[type='range']");
+
+  init() {
+    this.rangeInputEl.addEventListener('input', this.#updateProgressBar);
+  }
+
+  #updateProgressBar(event) {
+    const percentage = (event.target.value - 1) * 25;
+    event.target.style.setProperty('--progress', percentage);
+  }
+}
+
+const app = new App();
+app.init();

abd2ce3631b19d7376bafb807d627b75ca877d3d
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2023-05-11 20:01:01 -0400

    Added global style

--- interactive-rating-component/src/App.js
+++ interactive-rating-component/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap');
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+  }
+
+  img, picture, svg, video {
+    display: block;
+    max-width: 100%;
+  }
+
+  body {
+    --bg-color: hsl(216, 12%, 8%);
+
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
+    min-height: 100svh;
+    font-family: 'Overpass', sans-serif;
+    padding: 1.5em;
+    background-color: var(--bg-color);
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
